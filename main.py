7a33feaa62da6280e8a50e847bf73adf0dd2a87d
AdamHauptfeld <172135239+AdamHauptfeld@users.noreply.github.com>
2024-11-22 21:19:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -53,9 +53,11 @@ def main():
         st.write("You can also search for an entry or download the whole dataset as a csv.")
         book_table['original_copyright_year'] = book_table['original_copyright_year'].astype(str).str.replace(",", "")
         book_table.rename(columns={'original_copyright_year': 'year'}, inplace=True)
-        for entry in book_table['latest_listed_year']:
-            if entry is not None:
-                book_table['latest_listed_year'] = book_table['latest_listed_year'].astype(str).str.replace(",", "")
+        def clean_year(x):
+            if pd.notnull(x):
+                return str(x).replace(',', '')
+            return None
+        book_table['latest_listed_year'] = book_table['latest_listed_year'].apply(clean_year)
 
         #This displays the simplified table and also stores the row of any selection the user makes.
         selected_row = st.dataframe(book_table[['title', 'author', 'genre', 'year', 'keywords']],

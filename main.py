a9fad95d8132676cd2bc73f9ce8d2b4a1af13e2b
AdamHauptfeld <172135239+AdamHauptfeld@users.noreply.github.com>
2024-12-20 21:43:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def main():
         selected_page = option_menu(
             menu_title = None, 
             options = ['Dashboard', 'The Dataset', 'About the project'],
-            icons = [📊, 📃, 'book'],
+            icons = [:bar_chart:, :page_with_curl:, :book:],
             default_index=0
         )
     
